e84683e6a8fae97a9b032bb4770e14bd5946d017
rupeshkh-250 <rupeshkhairnar52@gmail.com>
2025-06-08 23:14:30 -0500

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch39 team with Santhosh, jay");
+      System.out.println("Hello Batch batch39 WITH FOR STUDENTS RUNNING in front of me and Tanuj, 3rd Commit Aditya in making CI automated vikas teach devops CI proecss");
+    }  
+} 
