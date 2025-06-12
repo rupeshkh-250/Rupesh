f39f7683c93d093b69691d6936c67fa719377d79
rupeshkh-250 <rupeshkhairnar52@gmail.com>
2025-06-12 01:26:31 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch39 team with Santhosh, jay");
-      System.out.println("Hello Batch batch39 WITH FOR STUDENTS RUNNING in front of me and Tanuj, 3rd Commit Aditya in making CI automated vikas teach devops CI proecss");
+     System.out.println("Hello world!!!!!");
+      System.out.println("This is first program by using java i want to integrate and deploy to the pipeline");
+        System.out.println("execute this code properly");
     }  
 } 
