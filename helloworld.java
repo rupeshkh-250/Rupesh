111341195fac38df32ff71d8f46bdf2fd800cd5f
rupeshkh-250 <rupeshkhairnar52@gmail.com>
2025-06-16 07:43:39 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,34 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello world!!!!!");
-      System.out.println("This is first program by using java i want to integrate and deploy to the pipeline");
-        System.out.println("execute this code properly");
-    }  
-} 
+// Java Code (Apache Beam)
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.values.KV;
+
+public class WordCount {
+
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create();
+        Pipeline pipeline = Pipeline.create(options);
+
+        pipeline.apply("ReadLines", TextIO.read().from("input.txt"))
+                .apply("ExtractWords", MapElements.via(new SimpleFunction<String, String>() {
+                    @Override
+                    public String apply(String line) {
+                        return line.toLowerCase().trim();
+                    }
+                }))
+                .apply("CountWords", Count.perElement())
+                .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String, Long>, String>() {
+                    @Override
+                    public String apply(KV<String, Long> wordCount) {
+                        return wordCount.getKey() + ": " + wordCount.getValue();
+                    }
+                }))
+                .apply("WriteCounts", TextIO.write().to("output.txt").withNumShards(1));
+        pipeline.run().waitUntilFinish();
+    }
+}
