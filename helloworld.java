65cbb7d7bfb7e2fe44dc27ec7d698f1b334870b9
Rupesh <rupeshkhairnar52@gmail.com>
2026-03-04 05:46:12 -0600

    updated code

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,12 @@ class Simple{
       System.out.println("This is just a testing program to check pipeline is working properly or not hello world");
         System.out.println("execute this code properly");
         System.out.println("welcome to code push to the git");
-
+      
         System.out.println("Hello welcome to jenkins pipelines !!!!");
+
+        var x = int 10;
+        var y= inst 20;
+        var z = x+y;
+        System.out.println("addition"+z);
     }  
 }
