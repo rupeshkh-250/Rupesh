469f62027b3f232022fbbebf80060c741b44b762
Rupesh <rupeshkhairnar52@gmail.com>
2026-05-02 22:16:52 -0500

    new code hai re mere bhai

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,11 @@ provider "aws" {
     region = "us-east-1" # specify the AWS region where you want to create the EC2 instance
 }
 resource "aws_instance" "this" {
-  ami                     = "ami-0ec10929233384c7f" # AMI ID for ubuntu 20.04 in us-east-1 region, you can change it based on your needs and region
+  ami                     = "ami-098e39bafa7e7303d" # AMI ID for ubuntu 20.04 in us-east-1 region, you can change it based on your needs and region
   instance_type           = "t2.micro"
- 
+  count = 1
+  tags = {
+    Name = "CSD-PIPS-CD-VM-01"
+  }
+  
 }
